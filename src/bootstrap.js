9fa530f8b39c9423dcf9d6ba9123f852d1f3832d
Elizabeth Pearce <eliz.mpearce@gmail.com>
2023-03-22 16:28:09 -0500

    building out search bar

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import thunk from 'redux-thunk';
 import Home from "./components/home";
 import Results from "./components/results"
 import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 import "./style/main.scss";
 
--- src/components/recent-posts.js
+++ src/components/recent-posts.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+import Post from './post';
 
-export default class RecentPosts extends Component {
+class RecentPosts extends Component {
+
+  componentDidMount() {
+    this.props.fetchRecentPosts();
+  }
+
+  renderPosts = function() {
+    const posts = this.props.recentPosts.map((post, index) => {
+      if(index <3) {      
+        return (
+          <Post {...post} key={index} />
+        )
+      } 
+    })
+
+    return posts
+  }
+
   render() {
     return (
       <div className='recent-posts'>
         <div className='recent-posts-wrapper'>
-          <div className='recent-posts-heading'></div>
+          <div className='recent-posts-heading'>Recent Posts</div>
           <ul className='recent-posts-posts'>
-            <li>recent posts 0</li>
-            <li>recent posts 1</li>
-            <li>recent posts 2</li>
+            {this.renderPosts()}
           </ul>
         </div>
-
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    recentPosts: state.posts.recentPosts
+  }
+}
+
+export default connect(mapStateToProps, actions)(RecentPosts);
